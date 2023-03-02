712681e658e46230b86d73334d9529ba2b01df91
Miloshev.M <miloshev.mitko@gmail.com>
2023-03-02 21:37:17 +0800

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@take-home-task/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
